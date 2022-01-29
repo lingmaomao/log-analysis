b40e7431d7c0a7b6b2ebd78509853632eca7dc8c
maomao.ling@intel.com <maomao.ling@intel.com>
2022-01-29 10:39:18 +0800

    finish analysis of idl.log and maintenance.log, write to .csv
    Signed-off-by: maomao.ling@intel.com <maomao.ling@intel.com>

--- getInfoFromTimeStamp.py
+++ getInfoFromTimeStamp.py
@@ -5,19 +5,10 @@ import csv
 
 # 1. 
 f = open('log.csv', 'w', encoding='utf-8', newline="")
-
 csv_writer = csv.writer(f)
 
-# 3. create table head
-#csv_writer.writerow(["name","age","male"])
-
-# 4. 写入csv文件内容
-#csv_writer.writerow(["l",'18','male'])
-#csv_writer.writerow(["c",'20','fe'])
-#csv_writer.writerow(["w",'22','fe'])
-
-# 5. 关闭文件
-
+#logs=["idl.log", "selelist.csv", "maintenance.log"]
+logs=["idl.log", "maintenance.log"]
 
 import os
 #import xlsxwriter as xw
@@ -27,13 +18,13 @@ import datetime
 
 #cmd='./test-run1.sh '+str(empu_num[l]) + ' ' + str(batch[k1])
 #grep "timestamp" converted_Tencent_FX21B050001PD_2022-01-17-09-55_RegRawData_1.json | awk '{print $2}'
-filename='converted_Tencent_FX21B050001PD_2022-01-17-09-55_RegRawData_1.json';
+
+#1.get timeStamp
+filename='/home/maomao/LOG-analysis/converted_Tencent_FX21B050001PD_2022-01-17-09-55_RegRawData_1.json';
 get_data_cmd='grep ' + 'timestamp '+ filename + '| awk \'{print $2}\''
 print("get_data_cmd: ", get_data_cmd)
-#get_data_cmd='cat ' + filename + '| grep "\<latency\>" | awk \'{sum+=$7} END {print sum/NR}\''
 latency = subprocess.check_output(get_data_cmd, shell=True)
-#latency=float(latency.strip())
-print("latency : ", latency)
+print("latency: ", latency)
 data=str(latency, 'utf-8')
 
 print ("latency[0]: ", latency[1:20])
@@ -41,25 +32,62 @@ print ("data[0]: ", data[1:20])
 time=data[1:20]
 #2022-01-08T09:25:57
 
-get_data_cmd='grep ' + time + ' onekeylog/log/ -rn' + '| grep idl'
-print("get_data_cmd: ", get_data_cmd)
-#grep 2022-01-08T09:25:57  -rn | grep idl
-latency = subprocess.check_output(get_data_cmd, shell=True)
-#latency=float(latency.strip())
-data=str(latency, 'utf-8')
-print("idl: ", data)
-#data=str(latency, 'utf-8')
-start=data.find('|')
-Diagnose_info=data[start:]
-print("Diagnose_info:", Diagnose_info)
+#2.extract info use timeStamp
+for l in range(len(logs)):
+    print("log-type: ", logs[l])
+    get_data_cmd='grep ' + time + ' /home/maomao/LOG-analysis/onekeylog/log/ -rn' + '| grep '+ logs[l]
+    print("get_data_cmd: ", get_data_cmd)
+    #grep 2022-01-08T09:25:57  -rn | grep idl
+    latency = subprocess.check_output(get_data_cmd, shell=True)
+    data=str(latency, 'utf-8')
+    print("data: ", data)
+    data.rstrip('\n')
+    print("data.rstrip(): ", data)
 
-data_slice=data.split(' ')
-print("data_slice:", data_slice)
-print("data_slice[3]:", data_slice[3])
-print("data_slice[4]:", data_slice[4])
-meachine=data_slice[3]
-InspurDiagnose=data_slice[4]
+    #ldl.log
+    if l == 0:
+        start=data.find('|')
+        Diagnose_info=data[start:]
+        print("Diagnose_info:", Diagnose_info)
 
-csv_writer.writerow([time,meachine,InspurDiagnose, Diagnose_info])
+        data_slice=data.split(' ')
+        print("data_slice:", data_slice)
+        print("data_slice[3]:", data_slice[3])
+        print("data_slice[4]:", data_slice[4])
+        meachine=data_slice[3]
+        InspurDiagnose=data_slice[4]
+        csv_writer.writerow([time, logs[l], meachine,InspurDiagnose, Diagnose_info])
 
+    #selelist.csv
+    #01/08/2022,09:25:57
+
+    #maintenance.log
+    if l == 1:
+        line_slice=data.split('\n')
+        print("line_slice:", line_slice)
+        print("line_slice.len:", len(line_slice))
+        print("line_slice[0]:", line_slice[0])
+        for m in range(len(line_slice)):
+            print("m=", m)
+            print("m.len=", len(line_slice[m]))
+            if len(line_slice[m])==0:
+                continue
+            print("line_slice[m]:", line_slice[m])
+            start=line_slice[m].find('|')
+            if start==-1:
+                start=line_slice[m].find('[')
+            print("start=", start)
+         
+            Diagnose_info=line_slice[m][start:]
+            print("Diagnose_info:", Diagnose_info)
+         
+            data_slice=line_slice[m].split(' ')
+            print("data_slice:", data_slice)
+            print("data_slice[3]:", data_slice[3])
+            print("data_slice[4]:", data_slice[4])
+            meachine=data_slice[3]
+            InspurDiagnose=data_slice[4]
+            csv_writer.writerow([time,logs[l], meachine,InspurDiagnose, Diagnose_info])
+         
 f.close()
+

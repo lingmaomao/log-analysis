0ef40cf13b6265707d0a7840a8f47643845e1c4a
maomao.ling@intel.com <maomao.ling@intel.com>
2022-03-14 15:06:32 +0800

    Sol log commit
    Signed-off-by: maomao.ling@intel.com <maomao.ling@intel.com>

--- readSol.py
+++ readSol.py
@@ -3,18 +3,8 @@ import os
 import time
 import datetime
 
-"""
-KEYS=[
-    "Linux version", #boot time,Jun 2 19:06:29 CST 2021
-    "microcode", #get line
-    "kernel panic", #get  line
-    "Error", #get line
-    "NMI" #get line
-]
-KEYS=[
-    "Linux version" #boot time,Jun 2 19:06:29 CST 2021
-]
-"""
+path='/home/maomao/LOG-analysis/Error_logs/Logs/'
+
 KEYS=[
     "Linux version",#boot time,Jun 2 19:06:29 CST 2021
     "microcode", #get line
@@ -25,7 +15,6 @@ KEYS=[
 
 res_file = "sol.csv"
 f = open(res_file, 'w', encoding='utf-8', newline="")
-#f = open(res_file, 'w', encoding='cp936', newline="")
 csv_writer = csv.writer(f)
 head_text=["filename"]
 head_text = head_text + KEYS 
@@ -34,24 +23,17 @@ csv_writer.writerow(head_text)
 info_set=[]
 
 def extractComp(filename, meachine):
-    #f = open(filename,'rb', encoding='cp936')
     f = open(filename,'rb')
     byt = f.readlines()
-    #print("byt:", byt)
-    #for every line
     info_set=[]
     info_set.append(meachine)
 
     for k in range(len(KEYS)):
         info=""
         for m in range(len(byt)):
-            #print("byt:", byt[m])
             text = byt[m].decode(errors='replace')
-            #print("text:", text)
             data0=text
-            #data0=str(byt[m], 'utf-8')
             if (data0.find(KEYS[k])!=-1):
-                #print("Data0:", data0)
                 if (KEYS[k]=="microcode"):
                     pos=data0.find("revision")
                     if (pos!=-1):
@@ -70,22 +52,16 @@ def extractComp(filename, meachine):
                             data0=data0[pos+prefix_lens:-1]
                         info=info+data0
                         print("info2:", info)
-        print("Info:", info)
+        #print("Info:", info)
         info_set.append(info)
 
-    print("info_set:", info_set)
+    #print("info_set:", info_set)
     csv_writer.writerow(info_set)
     
-path='/home/maomao/LOG-analysis/Error_logs/Logs/'
-#path='/home/maomao/LOG-analysis/Tencent_LC22113800013_2022-01-28-16-22'
 dirs=os.listdir(path)
 for m in range(len(dirs)):
-    filename = path+dirs[m]+'/onekeylog/runningdata/var/sollog/SOLHostCapture.log'
-    #filename = path+'/onekeylog/runningdata/var/sollog/SOLHostCapture.log'
-    #SOLHostCapture.log
+    filename = path + dirs[m] + '/onekeylog/runningdata/var/sollog/SOLHostCapture.log'
     print("filename:", filename)
-    #extractComp(filename, "dir")
     extractComp(filename, dirs[m])
-    #exit()
 
 f.close()
